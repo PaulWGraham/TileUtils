11d39410fb89779fff3bdc4e7079889e80c4a99f
Paul Graham <graham.paul+github@gmail.com>
2013-09-19 14:05:31 -0400

    Added test.tsx, test.ttt, and associated files.

---
+++ Tiles And Objects/Tilesets/test.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="test" tilewidth="32" tileheight="32">
+ <image source="../Images/test.png" width="64" height="64"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value="one"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="name" value="two"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="name" value="three"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="name" value="four"/>
+  </properties>
+ </tile>
+</tileset>
