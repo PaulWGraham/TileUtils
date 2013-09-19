2956d2e06ba2e57e7efd43d0faccaec598803787
Paul Graham <graham.paul+github@gmail.com>
2013-09-19 14:11:14 -0400

    Bumped TileUtils package version number to 0.0.2

--- TileUtils/setup.py
+++ TileUtils/setup.py
@@ -9,7 +9,7 @@ description = 	(	"TileUtils is an experimental proof of concept that explores th
 					"(http://www.mapeditor.org) into abstract data using a translation table.")
 
 setup(	name = 'TileUtils',
-		version = '0.0.1',
+		version = '0.0.2',
 		packages = ['TileUtils'],
 		scripts = [	'TileUtils/csvttt/csvttt.py', 'TileUtils/dirttt/dirttt.py',
 					'TileUtils/ertileplacer/ertileplacer.py', 'TileUtils/tileplacer/tileplacer.py',
